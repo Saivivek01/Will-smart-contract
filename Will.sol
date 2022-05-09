7cb32ec4aa58d791ddc16acaa0558645392c226f
Saivivek Patlola <84168546+Saivivek01@users.noreply.github.com>
2022-05-09 15:47:53 +0530

    Creating a smart contract

---
+++ Will.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.23;
+
+contract Inheritance{
+
+    address owner;
+    bool alive;
+    uint property;
+
+    constructor() public payable {
+        owner = msg.sender;
+        property = msg.value;
+        alive = false;
+    }
+
+    modifier realOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier isAlive {
+        require(alive = true);
+        _;
+    }
+
+    address[] wallets;
+
+    mapping (address => uint) inheritance;
+
+    function setup(address _wallet, uint _inheritance) public realOwner {
+        wallets.push(_wallet);
+        inheritance[_wallet] = _inheritance;
+    }
+
+    function propertyTransfer() private isAlive{
+        for(uint i=0;i<wallets.length;i++){
+            wallets[i].transfer(inheritance[wallets[i]]);
+        }
+    }
+
+    function died() public realOwner {
+        alive = true;
+        propertyTransfer();
+    }
+}
